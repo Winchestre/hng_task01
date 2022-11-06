5692b54ee454df1f5a61cfc4ecb49a11bfc34630
Solomon Bassey <solomonbassey20@gmail.com>
2022-11-06 19:11:31 +0100

    Feat: refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import Contact from './pages/Contact';
@@ -7,10 +7,10 @@ import Contact from './pages/Contact';
 const App = () => {
   return (
     <Router basename='/hng_task01'>
-      <Switch>
+      <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/contact' element={<Contact />} />
-      </Switch>
+      </Routes>
     </Router>
   )
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormInput = () => {
+  return (
+    <div className='formInput'>
+        <label>First Name</label>
+        <input type="text" placeholder='Enter your first name' />
+    </div>
+  )
+}
+
+export default FormInput
