191122dc3b0d90b4135bf2a81084234db20cc762
Solomon Bassey <solomonbassey20@gmail.com>
2022-11-06 19:01:24 +0100

    Feat: basename prop update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <>
-
-      <Router>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-      </Router>
-
-    </>
+    <Router basename='/hng_task01'>
+      <Switch>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/contact' element={<Contact />} />
+      </Switch>
+    </Router>
   )
 }
 
