affd19c28ac898033de6393b6bc37a01fab3cc60
Solomon Bassey <solomonbassey20@gmail.com>
2022-11-06 21:59:42 +0100

    Feat: Blank page fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<MainPage />} />
+    <BrowserRouter basename='/'>
+      <Routes >
         <Route path='/contact' element={<Contact />} />
+        <Route path='/' element={<MainPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
+   
   )
 }
 
